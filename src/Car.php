2f1435a045863c7e79c9fb0432dc91badc6831b2
Afton Downey and Jessica Fix <jcfix.pdx@gmail.com>
2016-02-11 08:29:35 -0800

    add car class

---
+++ src/Car.php
@@ -0,0 +1,65 @@
+<?php
+class Car
+{
+    private $make_model;
+    private $price;
+    private $miles;
+    private $image_path;
+
+
+    function __construct($car_model, $price, $mileage, $image)
+    {
+        $this->make_model = $car_model;
+        $this->price = $price;
+        $this->miles = $mileage;
+        $this->image_path = $image;
+    }
+
+    function setModel($car_model)
+    {
+        $this->make_model = $car_model;
+    }
+
+    function getModel()
+    {
+        return $this->make_model;
+    }
+
+    function setPrice($car_price)
+    {
+        $this->price = $price;
+    }
+
+    function getPrice()
+    {
+        return $this->price;
+    }
+
+    function setMileage($mileage)
+    {
+        $this->mileage = $mileage;
+    }
+
+    function getMileage()
+    {
+        return $this->mileage;
+    }
+
+    function setImage($car_image)
+    {
+        $this->image_path = $image;
+    }
+
+    function getImage()
+    {
+        return $this->image;
+    }
+
+
+    function worthBuying($max_price)
+    {
+        return $this->price < $max_price;
+    }
+}
+
+?>

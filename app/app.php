c7005abef6296289d652e391bbc9dcbbe8f0c02d
Afton Downey and Jessica Fix <jcfix.pdx@gmail.com>
2016-02-11 10:20:11 -0800

    Add delete function to clear postings list

--- app/app.php
+++ app/app.php
@@ -25,12 +25,12 @@
         return $app['twig']->render('confirmation.html.twig', array('newcar'=> $car));
     });
 
-    $app->post("/car_list", function() use ($app) {
+    $app->get("/car_list", function() use ($app) {
         return $app['twig']->render('car_list.html.twig', array('cars' => Car::getAll()));
     });
 
-    $app->post("/clear-list", function() use ($app) {
-      return $app['twig']->render('add_car.html.twig', array('cars'=> Car::deleteAll()));
+    $app->post("/clear_list", function() use ($app) {
+      return $app['twig']->render('car_list.html.twig', array('cars'=> Car::deleteAll()));
     });
 
 
